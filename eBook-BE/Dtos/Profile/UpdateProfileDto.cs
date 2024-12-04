a673e0cae5f181dd931e9962bff950a10c7b0645
DungLe2983 <lequocdung2983@gmail.com>
2024-12-04 18:29:02 +0700

    fix put Profile

--- eBook-BE/Dtos/Profile/UpdateProfileDto.cs
+++ eBook-BE/Dtos/Profile/UpdateProfileDto.cs
@@ -3,8 +3,8 @@
     public class UpdateProfileDto
     {
         public string UserName { get; set; }
-        public string OldPassword { get; set; }
-        public string NewPassword { get; set; }
+        public string? OldPassword { get; set; }
+        public string? NewPassword { get; set; }
         public string FullName { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
